ae3d186acc43481e4cdfe89fc4e08246cb351315
ghowe <gregory.howe@ingeniousideas.net>
2024-08-24 14:15:50 -0600

    Revert  4o-mini

--- types/llms.ts
+++ types/llms.ts
@@ -11,7 +11,6 @@ export type LLMID =
 // OpenAI Models (UPDATED 5/13/24)
 export type OpenAILLMID =
   | "gpt-4o" // GPT-4o
-  | "gpt-4o-mini" // GPT-4o-mini
   | "gpt-4-turbo-preview" // GPT-4 Turbo
   | "gpt-4-vision-preview" // GPT-4 Vision
   | "gpt-4" // GPT-4
