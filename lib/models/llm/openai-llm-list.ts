98fa5f5e8c7f62f52569a072f940b7c0de061d17
ghowe <gregory.howe@ingeniousideas.net>
2024-08-24 13:59:50 -0600

    Added gpt-4o-mini

--- lib/models/llm/openai-llm-list.ts
+++ lib/models/llm/openai-llm-list.ts
@@ -2,12 +2,26 @@ import { LLM } from "@/types"
 
 const OPENAI_PLATORM_LINK = "https://platform.openai.com/docs/overview"
 
-// OpenAI Models (UPDATED 1/25/24) -----------------------------
+// OpenAI Models (UPDATED 8/24/24) -----------------------------
 const GPT4o: LLM = {
   modelId: "gpt-4o",
   modelName: "GPT-4o",
   provider: "openai",
   hostedId: "gpt-4o",
+  platformLink: OPENAI_PLATORM_LINK,
+  imageInput: true,
+  pricing: {
+    currency: "USD",
+    unit: "1M tokens",
+    inputCost: 5,
+    outputCost: 15
+  }
+}
+const GPT4o: LLM = {
+  modelId: "gpt-4o-mini",
+  modelName: "GPT-4o-mini",
+  provider: "openai",
+  hostedId: "gpt-4o-mini",
   platformLink: OPENAI_PLATORM_LINK,
   imageInput: true,
   pricing: {
