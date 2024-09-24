4ad188f1dc7e3c85f2e375a4840848b70ae84a11
ghowe <gregory.howe@ingeniousideas.net>
2024-09-24 12:20:05 -0600

    Update chat-input.tsx added prompt to pick assistants

--- components/chat/chat-input.tsx
+++ components/chat/chat-input.tsx
@@ -277,7 +277,7 @@ export const ChatInput: FC<ChatInputProps> = ({}) => {
           className="ring-offset-background placeholder:text-muted-foreground focus-visible:ring-ring text-md flex w-full resize-none rounded-md border-none bg-transparent px-20 py-2 focus-visible:outline-none disabled:cursor-not-allowed disabled:opacity-50"
           placeholder={t(
             // `Ask anything. Type "@" for assistants, "/" for prompts, "#" for files, and "!" for tools.`
-            `Ask anything. Type @  /  #  !`
+            `Ask anything. Type "@" to select assistant and ask anything`
           )}
           onValueChange={handleInputChange}
           value={userInput}
